42ca34770aec3f16d19f3b68a1a76ce4d5cba44f
Shubhankar02 <shborade@gmail.com>
2019-01-17 12:32:11 +0530

    First commit

---
+++ app/models/category.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const categorySchema = new mongoose.Schema({
+    name: String,
+    desc: String,
+    subCategory: [{
+        id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "SubCategory"
+        },
+        name : String,
+        product: [{
+            id: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Product"
+            },
+            name: String,
+            desc: String,
+            price: Number
+        }]
+    }]
+});
+module.exports = mongoose.model('Category', categorySchema);
---
+++ app/models/subCategory.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const subCategorySchema = mongoose.Schema({
+    name : String,
+    // product: [{
+    //     id: {
+    //         type: mongoose.Schema.Types.ObjectId,
+    //         ref: "Product"
+    //     }
+    // }]
+});
+
+module.exports = mongoose.model('SubCategory', subCategorySchema);
